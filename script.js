3d517ecd99e42d26999d5c18faa0f1a0caa336dd
Andrew Lazariev <LazarievAndrew@users.noreply.github.com>
2019-07-19 22:33:53 +0300

    ver.1.2

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 var divListByTagName = document.getElementsByTagName('div');
 
-for (var i = 0; i < divListByTagName.length; i++){
-    if (i%2){
-        divListByTagName[i].innerText = 'Я четный элемент №' + (i + 1)/2;
+for (var i = 1; i <= divListByTagName.length; i++){
+    if (i % 2){
+        divListByTagName[i-1].innerText = 'Я нечетный элемент №' + (i + 1)/2;
     } else {
-        divListByTagName[i].innerText = 'Я нечетный элемент №' + (1 + i - (1 * i)/2);  // еще вариант ((1 + i)*2 - 1 * i)/2
+        divListByTagName[i-1].innerText = 'Я четный элемент №' + i/2; 
     }
 }
